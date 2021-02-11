75824e6d12798b374e00fa493b88cf912c1db5b6
Jari Aaltonen <jaritapani.aaltonen@gmail.com>
2021-02-11 12:56:29 +0200

    koe

---
+++ harjoitus2.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+
+eka_lista = [12,21,54,12,75]
+
+print(eka_lista)
+
+print(eka_lista[0])
+
+toka_lista = ["tämä","on","toinen","listamme"]
+
+print(toka_lista)
+print(toka_lista[0])
+
+toka_lista[1] = "ei ole"
+print(toka_lista)
+
+toka_lista.append("toimiiko")
+print(toka_lista)
+
+eka_lista.extend(toka_lista)
+print(eka_lista)
+
+kolmas_lista = eka_lista + toka_lista
+print(kolmas_lista)
+
+kolmas_lista[0] = eka_lista
+print(kolmas_lista)
+
+print(kolmas_lista[0][5]) #viitataan alaindeksiin
+
+print(toka_lista)
+eka_muuttuja = toka_lista.pop(0)
+print(toka_lista)
+
+for x in toka_lista:
+    print(x)
+
+for i in range (len(toka_lista)):
+    print(toka_lista[i])
+
+for i in range(30):
+    print("luku" + str(i))
+    if (i == 9):
+        break
+
+
+
---
+++ harjoitus3.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+def eka_funktio():
+    print("eka funktio tulostaa!")
+
+eka_funktio()
+
+def toka_funktio(teksti):
+    print("toka funktio tulostaa tämän + argumentin " + teksti)
+
+toka_funktio("ihan mitä vaan")
+toka_funktio("tällaista")
+
+#funktio palautuksella "return"
+def kolmas_funktio():
+    luku = 25
+    return luku
+
+funktion_palautus = kolmas_funktio()
+print(funktion_palautus)
